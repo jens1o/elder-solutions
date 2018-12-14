6909b7ddc09ee7984350203939639ae74ad85f58
Jens Hausdorf <mail@jens-hausdorf.de>
2018-12-14 12:30:41 +0100

    add(project-twenty-two): Solve problem 22.

---
+++ src/problem-twenty-two/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs;
+use std::time::SystemTime;
+
+fn main() {
+    let filename = "p022_names.txt";
+
+    let names = fs::read_to_string(filename).expect("Cannot read file.");
+
+    let benchmark_start = SystemTime::now();
+
+    // remove first and last character because they are just quote-marks
+    let (_, names) = names.split_at(1);
+    let (names, _) = names.split_at(names.len() - 1);
+
+    let mut names: Vec<&str> = names.split("\",\"").collect::<Vec<_>>();
+
+    // sort them alphabetically
+    names.sort();
+
+    let mut i = 0;
+    let mut score_sum = 0;
+
+    for name in names.iter() {
+        i += 1;
+
+        let score = name
+            .chars()
+            .map(|x| x as usize)
+            .map(|x| x - 64) // get numeric values (A = 1, code(A) = 65)
+            .sum::<usize>()
+            * i;
+
+        score_sum += score;
+    }
+
+    let benchmark_end = SystemTime::now().duration_since(benchmark_start).unwrap();
+
+    println!("Score of file: {}", score_sum);
+    println!("Calculation took: {:?}", benchmark_end);
+}
