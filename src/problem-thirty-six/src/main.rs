185853d87d2915b4a850ed3c5a464d2b866ead3e
Jens Hausdorf <mail@jens-hausdorf.de>
2018-12-16 13:40:54 +0100

    perf(problem-thirty-six): Avoid calculating every second number. Palindromes in binary representations MUST BE odd.

--- src/problem-thirty-six/src/main.rs
+++ src/problem-thirty-six/src/main.rs
@@ -1,16 +1,18 @@
 use std::time::SystemTime;
 
-const MAX_LIMIT: u32 = 1_000_000;
+const MAX_LIMIT: i32 = 1_000_000;
 
 fn main() {
     // Question: Is "1"(b) and "1"(decimal) a palindrome or not?
-    let mut number = 0;
+    let mut number: i32 = -1;
     let mut sum = 0;
 
     let benchmark_start = SystemTime::now();
 
-    while number != MAX_LIMIT {
-        number += 1;
+    while number <= MAX_LIMIT {
+        // they always must be odd, otherwise it is not possible
+        // to have a palindrome in binary representation
+        number += 2;
 
         if is_palindrome(&format!("{}", number)) {
             let binary_represenation = format!("{:b}", number);
