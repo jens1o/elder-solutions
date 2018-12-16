2dee4c6dccf5dfeff4a84cefaa00280fdfe3cf90
Jens Hausdorf <mail@jens-hausdorf.de>
2018-12-16 13:25:35 +0100

    add(problem-thirty-six): Solve problem 36.

---
+++ src/problem-thirty-six/src/main.rs
@@ -0,0 +1,34 @@
+use std::time::SystemTime;
+
+const MAX_LIMIT: u32 = 1_000_000;
+
+fn main() {
+    // Question: Is "1"(b) and "1"(decimal) a palindrome or not?
+    let mut number = 0;
+    let mut sum = 0;
+
+    let benchmark_start = SystemTime::now();
+
+    while number <= MAX_LIMIT {
+        number += 1;
+
+        if is_palindrome(&format!("{}", number)) {
+            let binary_represenation = format!("{:b}", number);
+
+            if is_palindrome(&binary_represenation) {
+                sum += number;
+                println!("Palindrom: {}, 0b{}", number, binary_represenation);
+            }
+        }
+    }
+
+    let benchmark_duration = SystemTime::now().duration_since(benchmark_start).unwrap();
+
+    println!("Sum: {}", sum);
+    println!("Took: {:?}", benchmark_duration);
+}
+
+#[inline(always)]
+fn is_palindrome(number: &str) -> bool {
+    number.chars().rev().collect::<Vec<_>>() == number.chars().collect::<Vec<_>>()
+}
