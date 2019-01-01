09acc42dafdc52bb0223a279fd9d55d3a2c4836a
Jens Hausdorf <mail@jens-hausdorf.de>
2019-01-01 20:42:39 +0100

    perf(problem-one-hundred-and-forty-five): Improve performance by threading.

--- src/problem-one-hundred-and-forty-five/src/main.rs
+++ src/problem-one-hundred-and-forty-five/src/main.rs
@@ -1,8 +1,20 @@
+#![feature(integer_atomics)]
+
+extern crate num_cpus;
+
+use std::cmp;
 use std::collections::HashSet;
 use std::env;
-use std::time::SystemTime;
+use std::io;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
+use std::time::{Duration, SystemTime};
 
-fn main() {
+/// calculating below this number is considered useless
+const START_NUMBER: u32 = 10;
+
+fn main() -> Result<(), io::Error> {
     let max_number: u32 = env::args()
         .nth(1)
         .and_then(|x| x.parse().ok())
@@ -11,12 +23,90 @@ fn main() {
 
     println!("Looking for reversible numbers below {}…", max_number);
 
-    let benchmark_start = SystemTime::now();
+    let number_of_threads = cmp::max(num_cpus::get(), 1);
 
+    println!("Launching {} threads.", number_of_threads);
+
+    let benchmark_start = SystemTime::now();
+    let mut thread_handles: Vec<JoinHandle<_>> = Vec::with_capacity(number_of_threads);
+    let reversible_numbers: Arc<Mutex<Vec<u32>>> =
+        Arc::new(Mutex::new(Vec::with_capacity(200 * number_of_threads)));
+
+    let finished_threads = Arc::new(Mutex::new(AtomicUsize::new(0)));
+
+    let reversible_numbers_to_calculate_per_thread: usize =
+        ((max_number - START_NUMBER) / (number_of_threads as u32)) as usize;
+
+    println!(
+        "Every thread needs to calculate ~{} numbers.",
+        reversible_numbers_to_calculate_per_thread
+    );
+
+    for thread_number in 1..=number_of_threads {
+        let start_position = cmp::max(
+            (thread_number - 1) * reversible_numbers_to_calculate_per_thread,
+            START_NUMBER as usize,
+        );
+
+        let current_end = start_position + reversible_numbers_to_calculate_per_thread;
+
+        let reversible_numbers = reversible_numbers.clone();
+        let finished_threads = finished_threads.clone();
+
+        thread_handles.push(thread::spawn(move || {
+            println!("Thread {} started.", thread_number);
+
+            let sub_results = find_reversible_number_below(
+                start_position as u32,
+                // if this is the last thread, calculate everything to the top, otherwise we would miss some because of rounding errors
+                if thread_number == number_of_threads {
+                    max_number
+                } else {
+                    current_end as u32
+                },
+            );
+
+            reversible_numbers.lock().unwrap().extend(sub_results);
+
+            finished_threads
+                .lock()
+                .unwrap()
+                .fetch_add(1, Ordering::SeqCst);
+        }));
+    }
+
+    loop {
+        thread::sleep(Duration::from_nanos(250));
+        let finished_threads = finished_threads.lock().unwrap().load(Ordering::SeqCst);
+        if finished_threads == number_of_threads {
+            break;
+        }
+    }
+
+    let benchmark_duration = SystemTime::now().duration_since(benchmark_start).unwrap();
+    println!();
+    println!("Calculation took {:?}.", benchmark_duration);
+
+    let reversible_numbers = reversible_numbers.lock().unwrap();
+
+    let mut reversible_numbers = reversible_numbers.iter().collect::<Vec<_>>();
+    reversible_numbers.sort();
+
+    println!(
+        "There are {} reversible numbers below {}.",
+        reversible_numbers.len(),
+        max_number,
+    );
+
+    Ok(())
+}
+
+fn find_reversible_number_below(start_number: u32, max_number: u32) -> HashSet<u32> {
     let mut reversible_numbers: HashSet<u32> = HashSet::with_capacity(200);
 
-    // checking for <= 10 is useless.
-    for number in 11..=max_number {
+    assert!(start_number < max_number);
+
+    for number in start_number..=max_number {
         let number_string = format!("{}", number);
 
         if number_string.ends_with('0') {
@@ -44,13 +134,5 @@ fn main() {
         }
     }
 
-    let benchmark_duration = SystemTime::now().duration_since(benchmark_start).unwrap();
-    println!();
-    println!("Calculation took {:?}.", benchmark_duration);
-
-    println!(
-        "There are {} reversible numbers below {}.",
-        reversible_numbers.len(),
-        max_number
-    );
+    reversible_numbers
 }
