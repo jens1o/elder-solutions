f5edbaf0ed5dafe2225dd9e981393c4cf7f2eac3
Jens Hausdorf <mail@jens-hausdorf.de>
2019-01-10 17:06:53 +0100

    add(problem-fifty-five): Solve problem 55.

---
+++ src/problem-fifty-five/src/main.rs
@@ -0,0 +1,74 @@
+use std::collections::HashSet;
+use std::env;
+use std::time::SystemTime;
+
+fn main() {
+    let max_number: u128 = env::args()
+        .nth(1)
+        .and_then(|x| x.parse::<_>().ok())
+        .and_then(|x| if x <= 10 { None } else { Some(x) })
+        .unwrap_or(10000);
+
+    println!("Generating lychrel numbers below {}.", max_number);
+
+    let benchmark_start = SystemTime::now();
+
+    let lychrel_numbers = get_lychrel_numbers(max_number);
+
+    let benchmark_duration = SystemTime::now().duration_since(benchmark_start).unwrap();
+
+    println!(
+        "There are {} lychrel numbers below {} (took {:?}).",
+        lychrel_numbers.len(),
+        max_number,
+        benchmark_duration
+    )
+}
+
+#[inline(always)]
+fn is_palindromic(number_string: String) -> bool {
+    number_string == number_string.chars().rev().collect::<String>()
+}
+
+#[inline(always)]
+fn reverse_number(number_string: String) -> u128 {
+    number_string
+        .chars()
+        .rev()
+        .collect::<String>()
+        .parse::<_>()
+        .unwrap()
+}
+
+fn get_lychrel_numbers(max: u128) -> HashSet<u128> {
+    assert!(max > 10);
+
+    let mut results = HashSet::with_capacity(max as usize / 3);
+
+    'outer: for i in 10..=max {
+        let mut current_iteration: u8 = 1;
+        let mut current_number: u128 = i;
+
+        while current_iteration < 50 {
+            current_iteration += 1;
+
+            let number_string = format!("{}", current_number);
+
+            let reversed_current_number = reverse_number(number_string);
+
+            let current_sum = current_number + reversed_current_number;
+
+            let current_sum_string = format!("{}", current_sum);
+
+            if is_palindromic(current_sum_string) {
+                results.insert(current_number);
+                continue 'outer;
+            } else {
+                current_number = current_sum;
+            }
+        }
+        // hit 50 iterations with the current
+    }
+
+    results
+}
